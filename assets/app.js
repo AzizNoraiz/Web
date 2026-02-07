552cc6952fbbcce3da6ec49f69b9788e06531732
AzizNoraiz <noraizaziz72@gmail.com>
2026-02-07 16:45:43 +0100

    HomePage v1

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,10 @@ import './stimulus_bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css';
+// 1. On charge le CSS de Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+// 2. On charge le JS de Bootstrap (Pour les menus déroulants, modales, etc.)
+import 'bootstrap';
+import './styles/app.css';
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
